cc06afed92127d73236e24afdbd62d27948b438b
camilac07 <camilacondeisla007@gmail.com>
2024-10-30 02:39:25 -0500

    initial commit

---
+++ src/main/java/com/pe/upc/smartgardenbackend/SmartGardenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pe.upc.smartgardenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartGardenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartGardenBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pe/upc/smartgardenbackend/SmartGardenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.upc.smartgardenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGardenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
