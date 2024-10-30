b7e223cb4f1c58c1097b31d78b8410d46bb5235f
camilac07 <camilacondeisla007@gmail.com>
2024-10-30 18:19:54 -0500

    fix: configuration

---
+++ src/main/java/com/pe/upc/smartgardenbackend/shared/infrastructure/documentation/openapi/config/OpenApiConfiguration.java
@@ -0,0 +1,41 @@
+package com.pe.upc.smartgardenbackend.shared.infrastructure.documentation.openapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI learningPlatformOpenApi() {
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title("Smart Garden Platform API")
+                        .description("Smart Garden Platform application REST API documentation.")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://springdoc.org")));
+
+        // Add security scheme
+
+        /*final String securitySchemeName = "bearerAuth";
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));*/
+
+        // Return OpenAPI configuration object with all the settings
+
+        return openApi;
+    }
+}
