c45fd2ec86e8e546b0a732b38468b5ab8cf129b8
camilac07 <camilacondeisla007@gmail.com>
2024-10-30 19:33:19 -0500

    feat(shared): add interface layer

---
+++ src/main/java/com/pe/upc/smartgardenbackend/shared/domain/model/aggregates/AuditableAbastractAgregateRoot.java
@@ -0,0 +1,36 @@
+package com.pe.upc.smartgardenbackend.shared.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditableAbastractAgregateRoot<T extends
+        AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
+
+    /**
+     * Registers a domain event.
+     *
+     * @param event the domain event to register
+     */
+    public void addDomainEvent(Object event) {
+        super.registerEvent(event);
+    }
+
+}
